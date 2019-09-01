f670b34d9e37d7158493c14896c1131a96c5ce63
Ashvini <ashvinideshmukh01@gmail.com>
2019-09-01 15:47:04 +0530

    mobility

---
+++ 7.Edgeledger_Website/js/main.js
@@ -0,0 +1,15 @@
+// var map;
+function initMap() {
+    const loc={
+        lat:42.361145,lng:-71.057083
+    };
+
+
+  const map = new google.maps.Map(document.querySelector('.map'), {
+//     //center: {lat: -34.397, lng: 150.644},
+//     zoom: 14
+//     center:loc
+//   });
+//   const maker= new google.maps.maker({position:loc,
+//     map:map});
+}
